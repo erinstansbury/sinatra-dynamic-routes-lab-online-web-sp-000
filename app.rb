6cd7ad858184bec22c30169b5e9decbfbf8be258
Erin Stansbury <ekstansbury@gmail.com>
2020-10-10 23:05:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
  get '/reversename/:name'
-  params[:name]
+  @user_name = params[:name]
+  rev = @user_name.reverse
+  "{#rev}"
  end
 
 end
