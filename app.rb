2df834937e1746641193a16567d2c6029393aea1
Erin Stansbury <ekstansbury@gmail.com>
2020-10-10 23:50:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    each 
+    @phrase.each do |ph|
     "#{phrase}"
   end
 
