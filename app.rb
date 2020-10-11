f02ef10c43199921483f9f9311862951c95e58cf
Erin Stansbury <ekstansbury@gmail.com>
2020-10-11 00:17:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ class App < Sinatra::Base
       "#{num1-num2}.to_s"
     elsif operation == 'multiply'
       "#{num1*num2}.to_s"
-    elsif operation == 'divide'
+    else operation == 'divide'
       "#{num1/num2}.to_s"
     end
   end
